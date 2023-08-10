12bebbc6a3e45332ef2fa9540126a5efb921d837
arsyaya <arsyajr9@gmail.com>
2023-08-10 16:24:29 +0700

    task day 4

---
+++ js/blog.js
@@ -0,0 +1,88 @@
+// Array
+// tabungan value kalian, dimana kalian bisa memiliki lebih dari satu value dalam satu variabel
+// let namaSiswa = ["Bagus", "Guswandi", "Arsya"];
+// console.log(namaSiswa);
+// console.log(namaSiswa[0]);
+// console.log(namaSiswa[1]);
+// console.log(namaSiswa[2]);
+
+// let nama = "Bagus";
+// let alamat = "Tangerang";
+// let umur = 24;
+
+// console.log(nama);
+// console.log(alamat);
+// console.log(umur);
+
+// object
+// tabungan variabel kalian, dimana kalian bisa memiliki lebh dari satu key value dalam satu variabel
+// contoh = nama: "Bagus", alamat: "Tangerang"
+// dimana nama sebagai key dan "Bagus" sebagai value
+// let dataPersonal1 = {
+//   nama: "Bagus",
+//   alamat: "Tangerang",
+//   umur: 24,
+// };
+
+// let dataPersonal2 = {
+//   nama: "Guswandi",
+//   alamat: "Padang",
+//   umur: 20,
+// };
+
+// let dataPersonal3 = {
+//   nama: "Arsya",
+//   alamat: "Karawang",
+//   umur: 17,
+// };
+
+// console.log(dataPersonal1);
+// console.log(dataPersonal2.nama);
+// console.log(dataPersonal3.umur);
+
+// Array of object
+// memperbolehkan kita menyimpan lebih dari satu object dalam satu variabel
+// let dataPersonal = [
+//   {
+//     nama: "Bagus",
+//     alamat: "Tangerang",
+//     umur: 24,
+//   },
+//   {
+//     nama: "Guswandi",
+//     alamat: "Padang",
+//     umur: 20,
+//   },
+//   {
+//     nama: "Arsya",
+//     alamat: "Karawang",
+//     umur: 17,
+//   },
+// ];
+
+// console.log(dataPersonal);
+// console.log(dataPersonal[1]);
+// console.log(dataPersonal[2].nama);
+
+// let data = [];
+
+// console.log(data);
+
+// let orang = [];
+
+// console.log(orang);
+
+// function addData() {
+//   let person = {
+//     nama: "Maulana",
+//     alamat: "Bandung",
+//     umur: 24,
+//   };
+
+//   // fungsi push adalah memasukkan data baru di dalam sebuah array
+//   // sebagai contoh,
+//   data.push(person);
+
+//   console.log(data);
+// }
+// addData();
